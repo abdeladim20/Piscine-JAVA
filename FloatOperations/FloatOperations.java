9afced84ff97090a1a4904a9ba37d62f3d7c2376
ajabbour <abdeladimjabbouri0@gmail.com>
2025-09-22 09:37:43 +0100

    working with float

---
+++ FloatOperations/FloatOperations.java
@@ -0,0 +1,20 @@
+public class FloatOperations {
+    public static float addTwoFloats(float a, float b) {
+        // your code here
+        return a + b;
+    }
+
+    public static float divideTwoFloats(float a, float b) {
+        // your code here
+        return a / b;
+    }
+}
+
+/*
+ * Add
+ * 3.5
+ * 83.46347
+ * Divide
+ * 3.5
+ * 0.8115942
+ */
