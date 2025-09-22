8999052b3ed78c30aaa363217264eaed1f091530
ajabbour <abdeladimjabbouri0@gmail.com>
2025-09-22 10:09:53 +0100

    working with enums and switch

---
+++ Chifoumi/Chifoumi.java
@@ -0,0 +1,21 @@
+package Chifoumi;
+
+public class Chifoumi {
+    public static ChifoumiAction getActionBeatenBy(ChifoumiAction chifoumiAction) {
+        // your code here
+        ChifoumiAction Action = switch (chifoumiAction) {
+            case ROCK -> chifoumiAction.SCISSOR;
+            case SCISSOR -> chifoumiAction.PAPER;
+            case PAPER -> chifoumiAction.ROCK;
+        };
+        return Action;
+    }
+}
+
+/*
+ * ROCK will return SCISSOR
+ * 
+ * PAPER will return ROCK
+ * 
+ * SCISSOR will return PAPER
+ */
