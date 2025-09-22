68e05c24d40168b14cdb1c7197078e11e6824324
ajabbour <abdeladimjabbouri0@gmail.com>
2025-09-22 11:28:39 +0100

    concatination

---
+++ StringConcat/StringConcat.java
@@ -0,0 +1,11 @@
+public class StringConcat {
+    public static String concat(String s1, String s2) {
+        // your code here
+        return s1 + s2;
+    }
+}
+
+/*
+Hello étudiant !
+Hello World !
+ */
