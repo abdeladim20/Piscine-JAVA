27776f4510828386bc92597fe292b1d815a9cd97
ajabbour <abdeladimjabbouri0@gmail.com>
2025-09-22 15:22:16 +0100

    Palindrome

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,11 +1,15 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
         // your code here
+        if (s == null) {
+            return false;
+        }
         String rev = "";
         for (int i = s.length()-1; i >= 0; i--) {
             rev += s.charAt(i);
         }
-        return rev.equals(s);
+
+        return rev.toLowerCase().equals(s.toLowerCase());
     }
 }
 
