547798ed4d15d2cc8885aeea579f483db92ac7e4
ajabbour <abdeladimjabbouri0@gmail.com>
2025-09-22 11:35:31 +0100

    handling null string

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,6 +1,9 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
         // your code here
+        if (s1 == null && s2 == null) {
+            return null;
+        }
         if (s1 == null) {
             s1 = "";
         }
