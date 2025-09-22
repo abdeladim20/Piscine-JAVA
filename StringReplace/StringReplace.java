69fd5244a5fa344c28938a7e6297c5820bb17285
ajabbour <abdeladimjabbouri0@gmail.com>
2025-09-22 12:08:31 +0100

    Replace

---
+++ StringReplace/StringReplace.java
@@ -0,0 +1,16 @@
+public class StringReplace {
+    public static String replace(String s, String target, String replacement) {
+        // your code here
+        return s.replace(target, replacement);
+    }
+    public static String replace(String s, char target, char replacement) {
+        // your code here
+        return s.replace(target, replacement);
+    }
+}
+
+/* 
+jevetpoint is e very good website
+my name was khan my name was java
+hey i'am java
+*/
