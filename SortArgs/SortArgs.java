7314a04328d7f1548f766e3c1bc4bd885804af99
ajabbour <abdeladimjabbouri0@gmail.com>
2025-09-23 10:55:43 +0100

    sort and print

---
+++ SortArgs/SortArgs.java
@@ -0,0 +1,21 @@
+public class SortArgs {
+    public static void sort(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            for (int j = i + 1; j < args.length; j++) {
+                int temp = 0;
+                if (Integer.parseInt(args[i]) > Integer.parseInt(args[j])) {
+                    temp = Integer.parseInt(args[i]);
+                    args[i] = args[j];
+                    args[j] = "" + temp;
+                }
+            }
+        }
+        for (int i = 0; i < args.length; i++) {
+            if (i < args.length - 1) {
+                System.out.print(args[i] + " ");
+            } else {
+                System.out.println(args[i]);
+            }
+        }
+    }
+}
