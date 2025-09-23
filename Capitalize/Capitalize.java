504f80df4c33896cc173f9f82f94d7b64f421223
ajabbour <abdeladimjabbouri0@gmail.com>
2025-09-23 15:45:50 +0100

    Capitalize the first and lowercase the other

--- Capitalize/Capitalize.java
+++ Capitalize/Capitalize.java
@@ -16,17 +16,18 @@ public class Capitalize {
             boolean firstLine = true;
 
             while ((line = reader.readLine()) != null) {
-
                 if (!firstLine) {
                     writer.newLine();
                 }
+
                 String[] words = line.trim().split("\\s+");
                 StringBuilder resultBuilder = new StringBuilder();
+
                 for (int i = 0; i < words.length; i++) {
                     String word = words[i];
                     if (!word.isEmpty()) {
                         resultBuilder.append(Character.toUpperCase(word.charAt(0)));
-                        resultBuilder.append(word.substring(1));
+                        resultBuilder.append(word.substring(1).toLowerCase());
                         if (i < words.length - 1) {
                             resultBuilder.append(" ");
                         }
