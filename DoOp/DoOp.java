f709c594d5ddba0c35e20546055845ebd295a8f9
ajabbour <abdeladimjabbouri0@gmail.com>
2025-09-23 10:24:10 +0100

    fix some issue

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -1,17 +1,16 @@
 public class DoOp {
     public static String operate(String[] args) {
         // your code here
-        if (args.length <= 2) {
+        if (args.length <= 2 || args.length > 3) {
             return "Error";
         }
         int num1 = Integer.parseInt(args[0]);
-        String operation_sign = args[1];
         int num2 = Integer.parseInt(args[2]);
-        String result = switch (operation_sign) {
+        String result = switch (args[1]) {
             case "-" -> String.valueOf(num1 - num2);
             case "+" -> String.valueOf(num1 + num2);
             case "*" -> String.valueOf(num1 * num2);
-            case "/" -> (num2 != 0) ? String.valueOf(num1 / 2) : "Error";
+            case "/" -> (num2 != 0) ? String.valueOf(num1 / num2) : "Error";
             case "%" -> (num2 != 0) ? String.valueOf(num1 % num2) : "Error";
             default -> "Error";
         };
