ec92e13dcd735ce788134bc7e6b9bbf93fb2d59d
ajabbour <abdeladimjabbouri0@gmail.com>
2025-09-24 15:18:55 +0100

    inheritence and more

---
+++ StarOverride/Star.java
@@ -0,0 +1,52 @@
+import java.util.Objects;
+
+public class Star extends CelestialObject {
+    private double magnitude;
+
+    public double getMagnitude() {
+        return magnitude;
+    }
+
+    public void setMagnitude(double magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    public Star(){
+        super();
+    }
+
+    public Star(String name, double x, double y, double z, double magnitude) {
+        super(name, x, y, z);
+        this.magnitude = magnitude;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s shines at the %.3f magnitude", getName(), magnitude);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+
+        Star otherObject = (Star) object;
+        return Double.compare(otherObject.getX(), getX()) == 0 &&
+                Double.compare(otherObject.getY(), getY()) == 0 &&
+                Double.compare(otherObject.getZ(), getZ()) == 0 &&
+                getName().equals(otherObject.getName()) && 
+                Double.compare(otherObject.magnitude, magnitude) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getX(), getY(), getZ(), magnitude);
+    }
+
+
+
+}
