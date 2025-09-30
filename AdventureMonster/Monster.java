077202901f4813db4c5dbd93ecca15c92fd6d15a
ajabbour <abdeladimjabbouri0@gmail.com>
2025-09-30 12:14:00 +0100

    format

---
+++ AdventureMonster/Monster.java
@@ -0,0 +1,16 @@
+
+public class Monster extends Character {
+
+    public Monster(String name, int currentHealth) {
+        super(name, currentHealth);
+    }
+
+    @Override
+    public String toString() {
+        if (this.getCurrentHealth() > 0) {
+            return String.format("%s is a monster with %d HP", this.getName(), this.getCurrentHealth());
+        }
+        return String.format("%s is a monster and is dead", this.getName());
+    }
+
+}
