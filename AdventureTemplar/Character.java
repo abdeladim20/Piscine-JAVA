dec29993c472b90dd2a56b69ce325abf7ad54603
ajabbour <abdeladimjabbouri0@gmail.com>
2025-10-01 10:10:20 +0100

    Templar

---
+++ AdventureTemplar/Character.java
@@ -0,0 +1,84 @@
+
+import java.util.*;
+
+public class Character {
+
+    private final int maxHealth;
+    private int currentHealth;
+    private final String name;
+    private static List<Character> allCharacters = new ArrayList<>();
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public void setCurrentHealth(int currentHealth) {
+        if (this.currentHealth + currentHealth > this.maxHealth) {
+            this.currentHealth = maxHealth;
+        } else {
+            this.currentHealth += currentHealth;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Character(String name, int maxHealth) {
+        this.currentHealth = maxHealth;
+        this.name = name;
+        this.maxHealth = maxHealth;
+        allCharacters.add(this);
+    }
+
+    @Override
+    public String toString() {
+        if (currentHealth == 0) {
+            return String.format("%s : KO", name);
+        }
+        return String.format("%s : %d/%d", name, currentHealth, maxHealth);
+    }
+
+    public void takeDamage(int damage) {
+        if (this.currentHealth - damage >= 0) {
+            this.currentHealth -= damage;
+        } else {
+            this.currentHealth = 0;
+        }
+    }
+
+    public void attack(Character character) {
+        character.takeDamage(9);
+    }
+
+    public static String printStatus() {
+        String dashes = "-".repeat(42);
+        if (allCharacters.isEmpty()) {
+            return String.format(dashes + "\nNobody's fighting right now !\n" + dashes + "\n");
+        }
+        String line1 = String.format(dashes + "\n" + "Characters currently fighting :\n");
+        String characLine = "";
+        for (Character character : allCharacters) {
+            characLine += " - " + character + "\n";
+        }
+        return line1 + characLine + dashes + "\n";
+    }
+
+    public static Character fight(Character character1, Character character2) {
+        while (character1.currentHealth > 0 && character2.currentHealth > 0) {
+            character1.attack(character2);
+            if (character2.currentHealth > 0) {
+                character2.attack(character1);
+            }
+        }
+        if (character1.currentHealth == 0) {
+            return character2;
+        }
+        return character1;
+    }
+
+}
---
+++ AdventureTemplar/Templar.java
@@ -0,0 +1,35 @@
+
+public class Templar extends Character implements Healer, Tank {
+
+    private final int healCapacity;
+    private final int shield;
+
+    public Templar(String name, int maxHealth, int healcapacity, int shield) {
+        super(name, maxHealth);
+        this.healCapacity = healcapacity;
+        this.shield = shield;
+    }
+
+    public int getHealCapacity() {
+        return this.healCapacity;
+    }
+    
+    public void heal(Character character) {
+        int health = character.getCurrentHealth() + this.healCapacity;
+        character.setCurrentHealth(health);
+    }
+
+    public int getShield() {
+        return this.shield;
+    }
+
+    @Override
+    public String toString() {
+        if (this.getCurrentHealth() > 0) {
+            return String.format("%s is a strong Templar with %d HP. It can heal %d HP and has a shield of %d.",
+                    getName(), this.getCurrentHealth(), this.healCapacity, this.shield);
+        }
+        return String.format("%s has been beaten, even with its %d shield. So bad, it could heal %d HP.",
+                this.getName(), this.shield, this.healCapacity);
+    }
+}
