90ba9fae0b6c61d3574251233973b2cbb3247490
ajabbour <abdeladimjabbouri0@gmail.com>
2025-10-02 09:53:51 +0100

    fix null issue

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -3,6 +3,9 @@ import java.util.List;
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
         // your code here
+        if (list1 == null || list2 == null) {
+            return false;
+        }
         return list1.equals(list2);
     }
 }
