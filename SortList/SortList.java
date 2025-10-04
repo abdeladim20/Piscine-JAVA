f74d64b7aa294d12ecd6c1f63faed0d649c6ea3d
ajabbour <abdeladimjabbouri0@gmail.com>
2025-10-04 10:18:14 +0100

    Sort List

---
+++ SortList/SortList.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortList {
+
+    public static List<Integer> sort(List<Integer> list) {
+        // your code here
+        List<Integer> list2 = new ArrayList<Integer>(list);
+        list2.sort(Comparator.naturalOrder());
+        return list2;
+    }
+
+    public static List<Integer> sortReverse(List<Integer> list) {
+        // your code here
+        List<Integer> list2 = new ArrayList<Integer>(list);
+        list2.sort(Comparator.reverseOrder());
+        return list2;
+    }
+}
+
+/*
+[-1, 1, 12, 14, 14, 15, 18, 54, 98]
+[98, 54, 18, 15, 14, 14, 12, 1, -1]
+ */
