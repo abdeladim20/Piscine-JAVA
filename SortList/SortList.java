94d69963686ae3abd774c9c4313f5317ee8fc52e
ajabbour <abdeladimjabbouri0@gmail.com>
2025-10-04 10:23:04 +0100

    handle reverse

--- SortList/SortList.java
+++ SortList/SortList.java
@@ -8,14 +8,14 @@ public class SortList {
     public static List<Integer> sort(List<Integer> list) {
         // your code here
         List<Integer> list2 = new ArrayList<Integer>(list);
-        list2.sort(Comparator.nullsFirst(Comparator.naturalOrder()));
+        list2.sort(Comparator.nullsLast(Comparator.naturalOrder()));
         return list2;
     }
 
     public static List<Integer> sortReverse(List<Integer> list) {
         // your code here
         List<Integer> list2 = new ArrayList<Integer>(list);
-        list2.sort(Comparator.nullsLast(Comparator.reverseOrder()));
+        list2.sort(Comparator.nullsFirst(Comparator.reverseOrder()));
         return list2;
     }
 }
