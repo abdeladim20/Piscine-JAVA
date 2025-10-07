c5932868300bcb8f1fd91c92886c2414357cc093
ajabbour <abdeladimjabbouri0@gmail.com>
2025-10-07 11:03:44 +0100

    parse date

---
+++ ParseDate/ParseDate.java
@@ -0,0 +1,38 @@
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class ParseDate {
+
+    public static LocalDateTime parseIsoFormat(String stringDate) {
+        // your code here
+        return LocalDateTime.parse(stringDate);
+    }
+
+    public static LocalDate parseFullTextFormat(String stringDate) {
+        // your code here
+        DateTimeFormatter formatter = new java.time.format.DateTimeFormatterBuilder()
+                .parseCaseInsensitive()
+                .appendPattern("EEEE dd MMMM yyyy")
+                .toFormatter(Locale.ENGLISH);
+        return LocalDate.parse(stringDate, formatter);
+    }
+
+    public static LocalTime parseTimeFormat(String stringDate) {
+        String cleanedString = stringDate.replaceAll("[^0-9 ]", "").trim();
+        String[] numbers = cleanedString.split("\\s+");
+
+        int hour = Integer.parseInt(numbers[0]);
+        int minute = Integer.parseInt(numbers[1]);
+        int second = Integer.parseInt(numbers[2]);
+
+        if (stringDate.contains("evening") && hour < 12) {
+            hour += 12;
+        }
+        return LocalTime.of(hour, minute, second);
+    }
+
+}
